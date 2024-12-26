9334287c084b28aa8fdd6e06ac2ad15f6dea6385
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2024-12-26 20:28:21 +0700

    update lifecycle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
+function Navbar() {
+  useEffect(() => {
+    console.log("Navbar component did mount");
+  }, []);
+  useEffect(() => {
+    return () => {
+      console.log("Navbar component will unmount");
+    };
+  }, []);
+  return (
+    <nav>
+      <h1>Navbar</h1>
+    </nav>
+  );
+}
+
 export default function Index({}: Props) {
-  const [a, setA] = useState(0);
-  function count() {
-    setA((prev) => ++prev);
-  }
-  console.log(a);
+  const [isShowNavbar, setIsShowNavbar] = useState<boolean>(true);
+  useEffect(() => {
+    console.log("isShowNavbar state has changed");
+  }, [isShowNavbar]);
+
   return (
     <div>
-      {a} <button onClick={() => count()}>click me</button>
+      {isShowNavbar && <Navbar />}
+      <button onClick={() => setIsShowNavbar(!isShowNavbar)}>Click me</button>
     </div>
   );
 }
+// import React, { Component } from "react";
+// type Props = {};
+
+// type State = {
+//   isShowNavbar: boolean;
+//   isShowButton: boolean;
+// };
+
+// class Navbar extends Component {
+//   componentDidMount(): void {
+//     console.log("Navbar component did mount");
+//   }
+
+//   componentWillUnmount(): void {
+//     console.log("Navbar component will unmount");
+//   }
+//   render() {
+//     return (
+//       <nav>
+//         <h1>Navbar</h1>
+//       </nav>
+//     );
+//   }
+// }
+
+// class Index extends React.Component<Props, State> {
+//   constructor(props: Props) {
+//     super(props);
+//     this.state = {
+//       isShowNavbar: true,
+//       isShowButton: true,
+//     };
+//   }
+
+//   componentDidUpdate(
+//     prevProps: Readonly<Props>,
+//     prevState: Readonly<State>,
+//     snapshot?: any
+//   ): void {
+//     if (this.state.isShowNavbar !== prevState.isShowNavbar) {
+//       console.log("isShowNavbar state has changed");
+//     }
+//   }
+
+//   count = () => {
+//     this.setState((prevState) => ({
+//       isShowNavbar: !prevState.isShowNavbar,
+//       isShowButton: !prevState.isShowButton,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         {this.state.isShowNavbar && <Navbar />}
+//         <p>{this.state.isShowNavbar}</p>
+//         <button onClick={this.count}>Click me</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Index;
