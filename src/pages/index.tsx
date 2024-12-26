2d677d2af5bb1c0c915f0f42b8ef663b90726fd3
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2024-12-26 21:08:11 +0700

    update get data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,51 @@ function Navbar({ duLieu }: NavbarProps) {
     </nav>
   );
 }
+
+function EmployeeList() {
+  const [employeeList, setEmployeeList] = useState<
+    {
+      createAt: string;
+      name: string;
+      avatar: string;
+      department: string;
+      gender: string;
+      id: number;
+    }[]
+  >([]);
+  useEffect(() => {
+    async function fetchEmployeeList() {
+      const response = await fetch(
+        "https://676d60880e299dd2ddff5d85.mockapi.io/employee"
+      );
+      const data = await response.json();
+      setEmployeeList(data);
+    }
+    fetchEmployeeList();
+  }, []);
+  return (
+    <div className="flex flex-col gap-5 divide-y-[1px]">
+      {employeeList.map((data) => (
+        <div className="py-2 flex flex-col gap-2" key={data.id}>
+          <h2 className="text-2xl text-green-400">{data.name}</h2>
+          <p>
+            department:{" "}
+            <span className="text-fuchsia-500">{data.department}</span>
+          </p>
+          <p>{data.gender}</p>
+          <div className="flex gap-3">
+            <button className="border p-1 px-2 rounded">update name</button>
+            <button className="border p-1 px-2 rounded">
+              update department
+            </button>
+            <button className="border p-1 px-2 rounded">update gender</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 type EmployeeType = {
   name: string;
   age: number;
@@ -59,6 +104,7 @@ export default function Index({}: Props) {
   };
   return (
     <div>
+      <EmployeeList />
       {isShowNavbar && <Navbar duLieu={navBarData} />}
       <button onClick={() => setIsShowNavbar(!isShowNavbar)}>Click me</button>
       <h1 className="font-mono text-2xl text-red-400">{employeeData.name}</h1>
