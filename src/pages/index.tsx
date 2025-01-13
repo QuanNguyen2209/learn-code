fba183b4439292989cdda7cf7b20fdccba883627
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2025-01-13 20:31:45 +0700

    update nv

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,12 @@ function Navbar({ duLieu }: NavbarProps) {
 }
 
 function EmployeeList() {
+  const [employeeName, setEmployeeName] = useState<string>("");
+  const [employeeDepartment, setEmployeeDepartment] = useState<string>("");
+  const [employeeGender, setEmployeeGender] = useState<"nam" | "nu" | "khac">(
+    "nam"
+  );
+
   const [employeeList, setEmployeeList] = useState<
     {
       createAt: string;
@@ -34,6 +40,7 @@ function EmployeeList() {
       id: number;
     }[]
   >([]);
+
   useEffect(() => {
     async function fetchEmployeeList() {
       const response = await fetch(
@@ -44,8 +51,72 @@ function EmployeeList() {
     }
     fetchEmployeeList();
   }, []);
+
+  async function handleCreateEmployee() {
+    const employeeData = {
+      name: employeeName,
+      department: employeeDepartment,
+      gender: employeeGender,
+    };
+
+    await fetch("https://676d60880e299dd2ddff5d85.mockapi.io/employee", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(employeeData),
+    }).then(async (result) => {
+      const newItemData = await result.json();
+      setEmployeeList([...employeeList, newItemData]);
+      alert("Tạo nhân viên thành công");
+    });
+  }
+
+  const handleDeleteEmployee = async (id: number) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this employee?"
+    );
+    if (confirmed) {
+      await fetch(
+        `https://676d60880e299dd2ddff5d85.mockapi.io/employee/${id}`,
+        {
+          method: "DELETE",
+        }
+      ).then(async (result) => {
+        alert("Xóa nhân viên thành công");
+        const removedItemData = await result.json();
+        setEmployeeList((prevList) =>
+          prevList.filter((employee) => removedItemData.id !== employee.id)
+        );
+      });
+    }
+  };
   return (
     <div className="flex flex-col gap-5 divide-y-[1px]">
+      <div className="flex gap-3 py-3">
+        <input
+          className="bg-black border-[1px] border-white rounded-md p-1"
+          placeholder="name..."
+          value={employeeName}
+          onChange={(e) => setEmployeeName(e.target.value)}
+        />
+        <input
+          className="bg-black border-[1px] border-white rounded-md p-1"
+          placeholder="department..."
+          value={employeeDepartment}
+          onChange={(e) => setEmployeeDepartment(e.target.value)}
+        />
+        <select
+          className="bg-black border-[1px] border-white rounded-md p-1"
+          value={employeeGender}
+          onChange={(e) =>
+            setEmployeeGender(e.target.value as "nam" | "nu" | "khac")
+          }
+        >
+          <option value="nam">Nam</option>
+          <option value="nu">Nu</option>
+          <option value="khac">Khac</option>
+        </select>
+        <button onClick={() => handleCreateEmployee()}>Tạo nhân viên</button>
+      </div>
       {employeeList.map((data) => (
         <div className="py-2 flex flex-col gap-2" key={data.id}>
           <h2 className="text-2xl text-green-400">{data.name}</h2>
@@ -60,6 +131,12 @@ function EmployeeList() {
               update department
             </button>
             <button className="border p-1 px-2 rounded">update gender</button>
+            <button
+              className="border p-1 px-2 rounded bg-red-500"
+              onClick={() => handleDeleteEmployee(data.id)}
+            >
+              delete
+            </button>
           </div>
         </div>
       ))}
@@ -105,6 +182,7 @@ export default function Index({}: Props) {
   return (
     <div>
       <EmployeeList />
+
       {isShowNavbar && <Navbar duLieu={navBarData} />}
       <button onClick={() => setIsShowNavbar(!isShowNavbar)}>Click me</button>
       <h1 className="font-mono text-2xl text-red-400">{employeeData.name}</h1>
@@ -132,66 +210,3 @@ export default function Index({}: Props) {
     </div>
   );
 }
-// import React, { Component } from "react";
-// type Props = {};
-
-// type State = {
-//   isShowNavbar: boolean;
-//   isShowButton: boolean;
-// };
-
-// class Navbar extends Component {
-//   componentDidMount(): void {
-//     console.log("Navbar component did mount");
-//   }
-
-//   componentWillUnmount(): void {
-//     console.log("Navbar component will unmount");
-//   }
-//   render() {
-//     return (
-//       <nav>
-//         <h1>Navbar</h1>
-//       </nav>
-//     );
-//   }
-// }
-
-// class Index extends React.Component<Props, State> {
-//   constructor(props: Props) {
-//     super(props);
-//     this.state = {
-//       isShowNavbar: true,
-//       isShowButton: true,
-//     };
-//   }
-
-//   componentDidUpdate(
-//     prevProps: Readonly<Props>,
-//     prevState: Readonly<State>,
-//     snapshot?: any
-//   ): void {
-//     if (this.state.isShowNavbar !== prevState.isShowNavbar) {
-//       console.log("isShowNavbar state has changed");
-//     }
-//   }
-
-//   count = () => {
-//     this.setState((prevState) => ({
-//       isShowNavbar: !prevState.isShowNavbar,
-//       isShowButton: !prevState.isShowButton,
-//     }));
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         {this.state.isShowNavbar && <Navbar />}
-//         <p>{this.state.isShowNavbar}</p>
-//         <button onClick={this.count}>Click me</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Index;
